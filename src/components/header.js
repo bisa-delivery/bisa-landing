3199b3532d175a706f66bb66919c2efb0e61ffab
Leonardy <bisa.delivery@gmail.com>
2020-04-19 20:08:02 +0700

    MVP 0 content
    Replaced texts and some styling for initial release

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `#F5D669`,
       marginBottom: `1.45rem`,
     }}
   >
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,14 +8,37 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h2>Kadang info di Instagram aja ngga cukup ☹️</h2>
+    <ul>
+      <li>Nulis di profil jadi nyemak, jadi cuma bisa di post</li>
+      <li>Linktree isinya minim banget, mau bikin website ribet :(</li>
+    </ul>
+    <h2>Branding langsung di depan 🚀</h2>
+    <ul>
+      <li>Linktr.ee/AB123 atau wa.me/62812xxx tidak cukup keren</li>
+      <li>Tapi kalau <a href="">kulacoffee.bisa.delivery</a> keren ngga tuh</li>
+      <li>Link bisa kemana aja, nambah WA, IG, Tokped, Grab, Gojek semua bisa</li>
+    </ul>
+    <h2>Gratisan dulu deh 😬 (apalagi lagi COVID)</h2>
+    <ul>
+      <li>Visi misinya gampang, yang mau jualan pokoknya dibikin gampang <i><b>dan keren</b></i></li>
+      <li>Kita bantuin set up juga nih, masukin link Instagram nya aja dulu: textfield</li>
+    </ul>
+    <h3>Yang sudah bikin duluan:</h3>
+      <ul>
+        <li><a href="">otten.bisa.delivery</a></li>
+        <li><a href="">otten.bisa.delivery</a></li>
+        <li><a href="">otten.bisa.delivery</a></li>
+      </ul>
+    <p>
+      Feedback? Curhat? Monggo di <a href="mailto:kamu@bisa.delivery">email</a> atau <a href="https://www.instagram.com/bisa.delivery/">DM</a> langsung
+    </p>
+
+    <Link to="/page-2/">Link</Link>
   </Layout>
 )
+// <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+//   <Image />
+// </div>
 
 export default IndexPage
